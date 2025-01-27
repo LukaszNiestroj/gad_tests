951abfd9075ecf4b30e094982cb2dbf9a54148bf
Łukasz Niestrój <lukasz.niestroj@tensquaregames.com>
2025-01-27 19:42:39 +0100

    chore: eslint settings

---
+++ eslint.config.mjs
@@ -0,0 +1,18 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { ignores: ['package-lock.json', 'playwright-report/**', 'test-results/**'] },
+  {files: ["**/*.{js,mjs,cjs,ts}"]},
+  {languageOptions: { globals: globals.node }},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    rules: {
+      'no-console': 'error',
+    },
+  },
+];
