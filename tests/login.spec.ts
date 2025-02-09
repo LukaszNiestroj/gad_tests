222be9cc9680bb68e4810ed2b78ca114094b6fb8
Łukasz Niestrój <lukasz.niestroj@tensquaregames.com>
2025-02-09 18:00:06 +0100

    chore: user data setup

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,5 +1,6 @@
 import { LoginPage } from '../src/pages/login.page';
 import { WelcomePage } from '../src/pages/welcome.page';
+import { testUser1 } from '../src/test-data/user-data';
 import { expect, test } from '@playwright/test';
 
 test.describe('Verify user login', () => {
@@ -9,8 +10,8 @@ test.describe('Verify user login', () => {
     async ({ page }) => {
       // Arrange
       const loginPage = new LoginPage(page);
-      const userEmail = 'Moses.Armstrong@Feest.ca';
-      const userPassword = 'test1';
+      const userEmail = testUser1.userEmail;
+      const userPassword = testUser1.userPassword;
 
       // ACT
       await loginPage.goto();
