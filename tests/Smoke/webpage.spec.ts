97aa35ca7ecd77db7e6f39398315ada57a080c07
Łukasz Niestrój <lukasz.niestroj@tensquaregames.com>
2025-05-03 14:50:23 +0200

    chore: own fixtures in smoke tests

--- tests/Smoke/webpage.spec.ts
+++ tests/Smoke/webpage.spec.ts
@@ -1,18 +1,13 @@
-import { ArticlesPage } from '@_src/pages/articles.page';
-import { CommentsPage } from '@_src/pages/comments.page';
-import { HomePage } from '@_src/pages/home.page';
-import { expect, test } from '@playwright/test';
+import { expect, test } from '@_src/fixtures/merge.fixture';
 
 test.describe('Verify service main page', () => {
   test(
     'Home page Title',
     { tag: ['@GAD-R01-01', '@HomePage', '@smoke'] },
-    async ({ page }) => {
+    async ({ homePage }) => {
       // Arrange
-      const homePage = new HomePage(page);
       const expectedHomePageTitle = 'GAD';
-      // Act
-      await homePage.goto();
+
       // Assert
       const title = await homePage.getTitle();
       expect(title).toContain(expectedHomePageTitle);
@@ -22,12 +17,9 @@ test.describe('Verify service main page', () => {
   test(
     'Articles page Title',
     { tag: ['@GAD-R01-02', '@smoke'] },
-    async ({ page }) => {
+    async ({ articlesPage }) => {
       // Arrange
-      const articlesPage = new ArticlesPage(page);
       const expectedArticlesTitle = 'Articles';
-      // Act
-      await articlesPage.goto();
       // Assert
       const title = await articlesPage.getTitle();
       expect(title).toContain(expectedArticlesTitle);
@@ -37,12 +29,9 @@ test.describe('Verify service main page', () => {
   test(
     'Comments page Title',
     { tag: ['@GAD-R01-02', '@smoke'] },
-    async ({ page }) => {
+    async ({ commentsPage }) => {
       // Arrange
-      const commentsPage = new CommentsPage(page);
       const expectedCommentsTitle = 'Comments';
-      // Act
-      await commentsPage.goto();
       // Assert
       const title = await commentsPage.getTitle();
       expect(title).toContain(expectedCommentsTitle);
