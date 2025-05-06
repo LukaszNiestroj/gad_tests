65e399e9b67fcaa15dd3766de29e19d6d430f41d
Łukasz Niestrój <lukasz.niestroj@tensquaregames.com>
2025-05-06 11:45:39 +0200

    chore: added utils waitForResponse function

--- tests/Integration/articles.spec.ts
+++ tests/Integration/articles.spec.ts
@@ -1,6 +1,6 @@
-import { RESPONSE_TIMEOUT } from '@_pw-config';
 import { prepareRandomArticle } from '@_src/factories/article.factory';
 import { expect, test } from '@_src/fixtures/merge.fixture';
+import { waitForResponse } from '@_src/utils/wait.util';
 
 test.describe('Verify articles', () => {
   test(
@@ -13,9 +13,7 @@ test.describe('Verify articles', () => {
       const articleData = prepareRandomArticle();
       articleData.title = '';
 
-      const responsePromise = page.waitForResponse('/api/articles', {
-        timeout: RESPONSE_TIMEOUT,
-      });
+      const responsePromise = waitForResponse(page, '/api/articles');
 
       // ACT
       await addArticlesView.createArticle(articleData);
@@ -32,19 +30,24 @@ test.describe('Verify articles', () => {
   test(
     'creating article with missing body text',
     { tag: ['@GAD-R04-01', '@logged'] },
-    async ({ addArticlesView }) => {
+    async ({ addArticlesView, page }) => {
       // Arrange
       const articleErrorMessage = 'Article was not created';
+      const expectedResponseCode = 422;
       const articleData = prepareRandomArticle();
       articleData.body = '';
 
+      const responsePromise = waitForResponse(page, '/api/articles');
+
       // ACT
       await addArticlesView.createArticle(articleData);
+      const response = await responsePromise;
 
       // Assert
       await expect(addArticlesView.articleErrorPopup).toContainText(
         articleErrorMessage,
       );
+      expect(response.status()).toBe(expectedResponseCode);
     },
   );
 
@@ -52,35 +55,45 @@ test.describe('Verify articles', () => {
     test(
       'creating article with title exceeding 128 signs',
       { tag: ['@GAD-R04-02', '@logged'] },
-      async ({ addArticlesView }) => {
+      async ({ addArticlesView, page }) => {
         // Arrange
         const articleErrorMessage = 'Article was not created';
         const articleData = prepareRandomArticle(129);
+        const expectedResponseCode = 422;
+
+        const responsePromise = waitForResponse(page, '/api/articles');
 
         // ACT
         await addArticlesView.createArticle(articleData);
+        const response = await responsePromise;
 
         // Assert
         await expect(addArticlesView.articleErrorPopup).toContainText(
           articleErrorMessage,
         );
+        expect(response.status()).toBe(expectedResponseCode);
       },
     );
 
     test(
       'creating article with title 128 signs',
       { tag: ['@GAD-R04-02', '@logged'] },
-      async ({ addArticlesView }) => {
+      async ({ addArticlesView, page }) => {
         // Arrange
         const articleData = prepareRandomArticle(128);
+        const expectedResponseCode = 201;
+
+        const responsePromise = waitForResponse(page, '/api/articles');
 
         // ACT
         const articlePage = await addArticlesView.createArticle(articleData);
+        const response = await responsePromise;
 
         // Assert
         await expect
           .soft(articlePage.articleTitle)
           .toHaveText(articleData.title);
+        expect(response.status()).toBe(expectedResponseCode);
       },
     );
   });
