6b1bb6fd750bcef029bbc6043ed5f2fc3d2b1c87
Łukasz Niestrój <lukasz.niestroj@tensquaregames.com>
2025-05-21 12:20:19 +0200

    test: api creation of article

--- tests/api/articles.integration.spec.ts
+++ tests/api/articles.integration.spec.ts
@@ -1,5 +1,6 @@
 import { prepareRandomArticle } from '@_src/factories/article.factory';
 import { expect, test } from '@_src/fixtures/merge.fixture';
+import { testUser1 } from '@_src/test-data/user-data';
 
 test.describe('Verify articles CRUD operations', () => {
   test(
@@ -24,6 +25,58 @@ test.describe('Verify articles CRUD operations', () => {
 
       // Assert
       expect(response.status()).toBe(expectedStatusCode);
+    },
+  );
+  test(
+    'should create an article with a logged-in user',
+    { tag: ['@GAD-R09-01', '@integration', '@api'] },
+    async ({ request }) => {
+      // Arrange
+      const expectedStatusCode = 201;
+
+      // Login as a user
+      const loginUrl = '/api/login';
+
+      const userData = {
+        email: testUser1.userEmail,
+        password: testUser1.userPassword,
+      };
+      const responseLogin = await request.post(loginUrl, {
+        data: userData,
+      });
+      const responseLoginJson = await responseLogin.json();
+      const token = responseLoginJson.access_token;
+
+      // Act
+      const articlesUrl = '/api/articles';
+      const randomArticleData = prepareRandomArticle();
+      const articleData = {
+        title: randomArticleData.title,
+        body: randomArticleData.body,
+        date: '2025-05-21T09:34:59.086Z',
+        image:
+          '.\\data\\images\\256\\team-testers_c4a246ec-8a7f-4f93-8b3a-2bc9ae818bbc.jpg',
+      };
+
+      const headers = {
+        Authorization: `Bearer ${token}`,
+      };
+
+      const responseArticle = await request.post(articlesUrl, {
+        headers,
+        data: articleData,
+      });
+
+      // Assert
+      const actualResponseStatus = responseArticle.status();
+      expect(
+        actualResponseStatus,
+        `Status code expected ${expectedStatusCode}, but received ${actualResponseStatus}`,
+      ).toBe(expectedStatusCode);
+
+      const article = await responseArticle.json();
+      expect.soft(article.title).toEqual(articleData.title);
+      expect.soft(article.body).toEqual(articleData.body);
     },
   );
 });
