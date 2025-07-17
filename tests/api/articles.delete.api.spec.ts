33545af3048eced71c93427d2d624c28c1db2ffa
LukaszNiestroj <lukas.niestroj@gmail.com>
2025-07-17 13:04:19 +0200

    chore: refactor split article and comment integration tests

--- tests/api/articles.delete.api.spec.ts
+++ tests/api/articles.delete.api.spec.ts
@@ -8,23 +8,10 @@ import { apiUrls } from '@_src/api/utils/api.util';
 import { expect, test } from '@_src/ui/fixtures/merge.fixture';
 import { APIResponse } from '@playwright/test';
 
-test.describe('Verify articles CRUD operations', () => {
-  test(
-    'should not create an article without a logged-in user',
-    { tag: ['@GAD-R09-01', '@integration', '@crud'] },
-    async ({ request }) => {
-      // Arrange
-      const expectedStatusCode = 401;
-      const articleData = prepareArticlePayload();
-      // Act
-      const response = await request.post(apiUrls.articlesUrl, {
-        data: articleData,
-      });
-      // Assert
-      expect(response.status()).toBe(expectedStatusCode);
-    },
-  );
-  test.describe('CRUD operations', { tag: ['@crud'] }, () => {
+test.describe(
+  'Verify articles DELETE operations',
+  { tag: ['@api', '@article', '@crud', '@delete'] },
+  () => {
     let responseArticle: APIResponse;
     let headers: Headers;
     let articleData: ArticlePayload;
@@ -42,24 +29,6 @@ test.describe('Verify articles CRUD operations', () => {
       );
     });
 
-    test(
-      'should create an article with a logged-in user',
-      { tag: ['@GAD-R09-01'] },
-      async () => {
-        // Arrange
-        const expectedStatusCode = 201;
-        // Assert
-        const actualResponseStatus = responseArticle.status();
-        expect(
-          actualResponseStatus,
-          `expect status code ${expectedStatusCode}, and received ${actualResponseStatus}`,
-        ).toBe(expectedStatusCode);
-
-        const articleJson = await responseArticle.json();
-        expect.soft(articleJson.title).toEqual(articleData.title);
-        expect.soft(articleJson.body).toEqual(articleData.body);
-      },
-    );
     test(
       'should delete an article with a logged-in user',
       { tag: ['@GAD-R09-03'] },
@@ -120,5 +89,5 @@ test.describe('Verify articles CRUD operations', () => {
         );
       },
     );
-  });
-});
+  },
+);
